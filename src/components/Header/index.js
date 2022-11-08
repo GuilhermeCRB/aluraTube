69193611ffd0a885f7c03294183f6bcba73898d3
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-11-07 23:35:54 -0300

    feat: adds banner

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import { StyledHeader } from '../../style/Header';
+import { Banner, StyledHeader } from '../../style/Header';
 
 export default function Header({ config }) {
   return (
     <StyledHeader>
+      <Banner picture="./imgs/banner.jpg"></Banner>
       <section className="user-info">
-        <img src={`https://github.com/${config.github}.png`} alt="Profile picture" />
+        <img className="profile" src={`https://github.com/${config.github}.png`} alt="Profile picture" />
         <div>
           <h2>{config.name}</h2>
           <p>{config.description}</p>
--- src/style/Header/index.js
+++ src/style/Header/index.js
@@ -2,17 +2,25 @@ import styled from 'styled-components';
 
 export const StyledHeader = styled.div`
   overflow: hidden;
-  img {
+  .profile {
     width: 80px;
     height: 80px;
     border-radius: 50%;
   }
   .user-info {
-    margin-top: 50px;
+    margin-top: 10px;
     display: flex;
     align-items: center;
     width: 100%;
     padding: 16px 32px;
     gap: 16px;
   }
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  height: 200px;
+  margin-top: 56px;
+  background-image: ${({ picture }) => `url(${picture})`};
+  background-size: cover;
 `;
